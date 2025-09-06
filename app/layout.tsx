b5207a7964e96af817b8c0810bf956106e97a0a0
Cary <carysullivan@Carys-MacBook-Air-2.local>
2025-09-05 21:42:41 -0400

    updated call buttons

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo-svg.svg" type="image/svg+xml" />
+      </head>
       <body className={inter.className}>
         {children}
       </body>
